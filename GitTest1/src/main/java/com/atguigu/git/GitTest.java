b0ca299359ce9eab5effa68cad0f7bc8ec7a40c4
tom_glb <goodMorning_glb@aiguigu.com>
2022-01-16 15:35:14 +0800

    first git test

---
+++ GitTest1/src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @author p1sces
+ * @Descirption
+ * @create 2022-01-16 15:34
+ */
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+    }
+}
