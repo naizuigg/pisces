4972fc5d56b5baf2644e7588ee32f38649fc784d
tom_glb <goodMorning_glb@aiguigu.com>
2022-01-16 15:36:45 +0800

    second git test

--- GitTest1/src/main/java/com/atguigu/git/GitTest.java
+++ GitTest1/src/main/java/com/atguigu/git/GitTest.java
@@ -2,13 +2,12 @@ package com.atguigu.git;
 
 /**
  * @author p1sces
- * @Descirption
- * @create 2022-01-16 15:34
  */
 
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git！");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
